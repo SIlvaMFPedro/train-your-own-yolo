098f006a1831ca07b5970a5da3443d70cc5ce1a3
silvamfpedro <silva.mfpedro@gmail.com>
2020-09-24 17:10:53 +0100

    Added .gitattributes

--- example.py
+++ example.py
@@ -54,7 +54,7 @@ if not os.path.isfile(os.path.join(model_folder, "trained_weights_final.h5")):
 detector_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), "3_Inference", "Detector.py")
 result_file = os.path.join(output_folder, "Detection_Results.csv")
 model_weights = os.path.join(model_folder, "trained_weights_final.h5")
-classes_file = os.path.join(model_folder, "data_classes.txt")
+classes_file = os.path.join(model_folder, "data_classes.txt.txt")
 anchors = os.path.join(root_folder, "2_Training", "src", "keras_yolo3", "model_data", "yolo_anchors.txt")
 
 arglist = [
